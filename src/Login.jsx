37459a465e4e83d7ddbb2b7867cd559d102e5376
Chris <cshdevelopment1@gmail.com>
2021-04-17 01:42:05 -0400

    Next

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,7 +6,9 @@ function Login() {
         <div className="login">
             <img src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c528.png" alt="logo"/>
 
-            <form></form>
+            <form>
+                <input placeholder="Full name (required if registering)" type="text"/>
+            </form>
         </div>
     )
 }
